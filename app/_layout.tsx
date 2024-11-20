403425775bb86d52d1ff14d283ece280b7204b4c
kys0017 <kys0017@gmail.com>
2024-11-21 01:27:09 +0900

    ✨ 2.2 AppLoading part Two - preloading fonts and assets using loadAsync

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,47 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
+import {Stack} from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import {StatusBar} from 'expo-status-bar';
+import {useEffect} from 'react';
 import 'react-native-reanimated';
+import {Asset} from 'expo-asset';
+import * as Font from 'expo-font';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import {Image} from 'react-native'
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {useColorScheme} from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  const [loaded] = Font.useFonts({
+    "SpaceMono": require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
   useEffect(() => {
     if (loaded) {
+      Font.loadAsync(Ionicons.font)
+      Asset.loadAsync(require('./../coffee.jpg'))
+      Image.prefetch('https://media.istockphoto.com/id/1366930330/ko/%EC%82%AC%EC%A7%84/%ED%9D%B0%EC%83%89-%EB%B0%B0%EA%B2%BD%EC%97%90-%EA%B3%A0%EB%A6%BD-%EB%90%9C-%EC%86%90%EC%97%90-%EB%9C%A8%EA%B1%B0%EC%9A%B4-%EC%BB%A4%ED%94%BC-%EC%BB%B5.jpg?s=2048x2048&w=is&k=20&c=KKdy4ZIUyTB0GCcOiOTlMOr9jahEIGf5vAMjiR-0nD4=');
       SplashScreen.hideAsync();
+      console.log('loaded!!')
     }
   }, [loaded]);
 
   if (!loaded) {
+    console.log('not loaded!!')
     return null;
   }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+        <Stack.Screen name="+not-found"/>
       </Stack>
-      <StatusBar style="auto" />
+      <StatusBar style="auto"/>
     </ThemeProvider>
   );
 }
