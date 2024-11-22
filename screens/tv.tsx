73a10fabe9a3dbcab7bde50787891dec9e64e567
kys0017 <kys0017@gmail.com>
2024-11-22 18:49:11 +0900

    ✨ 2.5 Tab Navigation

---
+++ screens/tv.tsx
@@ -0,0 +1,7 @@
+import {View, Text} from "react-native";
+
+const Tv = () => {
+  return <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}><Text>Tv</Text></View>
+}
+
+export default Tv
