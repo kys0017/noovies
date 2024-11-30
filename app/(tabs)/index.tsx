19d3987cb302ae115b479f1e5670235f30274e21
kys0017 <kys0017@gmail.com>
2024-11-30 20:19:54 +0900

    ✨ 3.2 Movies Screen part One - add swiper, fetch not playing data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,48 @@
-import {Text} from 'react-native';
-import {useRouter} from "expo-router";
-import React from "react";
-import styled from 'styled-components/native'
+import styled from "styled-components/native";
+import Swiper from "react-native-web-swiper";
+import { Dimensions } from "react-native";
 
-const Btn = styled.TouchableOpacity`
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-    background-color: ${props => props.theme.mainColor};
+const API_KEY = 'd2212e9338e21899f29c5010882d13d1'
+
+const API_ACC_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMjIxMmU5MzM4ZTIxODk5ZjI5YzUwMTA4ODJkMTNkMSIsIm5iZiI6MTYzMzA4MDIzNC40ODE5OTk5LCJzdWIiOiI2MTU2ZDNhYTE1NmNjNzAwMmMxZjNkNjEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.3vKKFH7_CaHANmn6GNVJ_9QfzZJg6gdaqgFej8CqE78'
+
+const Container = styled.ScrollView`
+  background-color: ${prop => prop.theme.mainBgColor};
 `
 
-const Title = styled.Text<{ selected?: boolean }>`
-    color: ${props => props.theme.textColor}
+const View = styled.View`
+  flex: 1;
 `
 
-export default function HomeScreen() {
-  const router = useRouter();
+const { height: SCREEN_HEIGHT } = Dimensions.get("window")
 
-  return (
-    <Btn onPress={() => router.push('/one')}>
-      <Text>Home</Text>
-      <Title>Go to One</Title>
-    </Btn>
-  );
+const Movies = () => {
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${API_ACC_TOKEN}`
+    }
+  };
+
+  fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+
+  const getNowPlaying = () => {
+  }
+
+  return <Container>
+    <Swiper loop timeout={3.5} controlsEnabled={false} containerStyle={{ width: '100%', height: SCREEN_HEIGHT / 4 }}>
+      <View style={{ backgroundColor: 'red' }}></View>
+      <View style={{ backgroundColor: 'blue' }}></View>
+      <View style={{ backgroundColor: 'red' }}></View>
+      <View style={{ backgroundColor: 'blue' }}></View>
+    </Swiper>
+  </Container>
+
 }
+
+export default Movies;
--- screens/tv.tsx
+++
@@ -1,7 +0,0 @@
-import {View, Text} from "react-native";
-
-const Tv = () => {
-  return <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}><Text>Tv</Text></View>
-}
-
-export default Tv
