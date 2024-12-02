7d30047de10b3562bd1aaae867fec6a491c2a0e5
kys0017 <kys0017@gmail.com>
2024-12-02 20:54:25 +0900

    ✨ 3.7 Coming soon

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -44,6 +44,31 @@ const Votes = styled.Text`
   color: rgba(255, 255, 255, .8);
   font-size: 10px;
 `
+const ListContainer = styled.View`
+  margin-bottom: 40px;
+`
+const HMovie = styled.View`
+  padding: 0 30px;
+  margin-bottom: 30px;
+  flex-direction: row;
+`
+const HColumn = styled.View`
+  margin-left: 15px;
+  width: 80%;
+`
+const Overview = styled.Text`
+  color: white;
+  opacity: .8;
+  width: 80%;
+`
+const Release = styled.Text`
+  color: white;
+  font-size: 12px;
+  margin-vertical: 10px
+`
+const ComingSoonTitle = styled(ListTitle)`
+  margin-bottom: 30px;
+`
 
 const Movies = () => {
   const [loading, setLoading] = useState(true)
@@ -103,22 +128,41 @@ const Movies = () => {
         originalTitle={movie.original_title}
         key={movie.id} />)}
     </Swiper>
-    <ListTitle>
-      Trending Movies
-    </ListTitle>
-    <TrendingScroll
-      contentContainerStyle={{ paddingLeft: 30 }}
-      horizontal
-      showsHorizontalScrollIndicator={false}>
-      {trending.map((movie: any) => <Movie key={movie.id}>
-        <Poster path={movie.poster_path} />
+    <ListContainer>
+      <ListTitle>Trending Movies</ListTitle>
+      <TrendingScroll
+        contentContainerStyle={{ paddingLeft: 30 }}
+        horizontal
+        showsHorizontalScrollIndicator={false}>
+        {trending.map((movie: any) => <Movie key={movie.id}>
+          <Poster path={movie.poster_path} />
+          <Title>
+            {movie.original_title.slice(0, 13)}
+            {movie.original_title.length > 13 ? '...' : null}
+          </Title>
+          <Votes>
+            {movie.vote_average > 0 ?
+              `⭐️${movie.vote_average}/10` : `Coming soon`
+            }
+          </Votes>
+        </Movie>)}
+      </TrendingScroll>
+    </ListContainer>
+    <ComingSoonTitle>Coming soon</ComingSoonTitle>
+    {upcoming.map((movie: any) => <HMovie key={movie.id}>
+      <Poster path={movie.poster_path} />
+      <HColumn>
         <Title>
-          {movie.original_title.slice(0, 13)}
-          {movie.original_title.length > 13 ? '...' : null}
+          {movie.original_title}
         </Title>
-        <Votes>⭐️{movie.vote_average}/10</Votes>
-      </Movie>)}
-    </TrendingScroll>
+        <Release>
+          {new Date(movie.release_date).toLocaleDateString('ko', { month: 'long', day: 'numeric', year: 'numeric' })}
+        </Release>
+        <Overview>
+          {(!!movie.overview && movie.overview.length > 140) ? `${movie.overview.slice(0, 140)}...` : movie.overview}
+        </Overview>
+      </HColumn>
+    </HMovie>)}
   </Container>
 
 }
